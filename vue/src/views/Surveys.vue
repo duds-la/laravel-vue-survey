17fe80fbc5fdc9f85fdc626e19aa562de3fb0f4e
Eduardo Lazzaretti <duadsal@gmail.com>
2023-12-28 20:47:26 -0300

    vue components login, regist, layout

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,20 +1,20 @@
 <template>
-    <header class="bg-white shadow">
+  <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+          <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
         </div>
       </header>
       <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            contenst here
+          contenst here
         </div>
       </main>
 </template>
 <script>
 export default {
-    name: 'Dashboard'
+  name: 'Surveys'
 }
 </script>
 <style scoped>
-    
+  
 </style>
