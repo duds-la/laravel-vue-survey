5618349db0a846ac2309b3d5c13e0162825b0952
Eduardo Lazzaretti <duadsal@gmail.com>
2023-12-28 22:50:11 -0300

    crete latout and navbar

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,13 +3,23 @@ import {createStore} from "vuex";
 const store = createStore({
     state: {
         user:{
-            data: {},
+            data: {
+                name: 'Tom Cook',
+                email: 'tom@example.com',
+                imageUrl:
+                'https://images7.alphacoders.com/683/683330.jpg',
+            },
             token: 123,
         }
     },
     getters: {},
     actions: {},
-    mutations: {},
+    mutations: {
+        logout: state => {
+            state.user.data ={};
+            state.user.token = null;
+        }
+    },
     modules: {}
 });
 
