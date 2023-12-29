63b12e00651f83dda7830ca7d424736607bfc620
Eduardo Lazzaretti <duadsal@gmail.com>
2023-12-29 19:59:41 -0300

    sign up fronted

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -4,20 +4,38 @@ const store = createStore({
     state: {
         user:{
             data: {
-                name: 'Tom Cook',
-                email: 'tom@example.com',
-                imageUrl:
-                'https://images7.alphacoders.com/683/683330.jpg',
+                
             },
-            token: 123,
+            token: sessionStorage.getItem('TOKEN'),
         }
     },
     getters: {},
-    actions: {},
+    actions: {
+        register({commit}, user){
+            return fetch(`http://localhost:8000/api/register`,{
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                method: "POST",
+                body: JSON.stringify(user),
+            })
+            .then((res) => res.json())
+            .then((res) => {
+                commit("setUser", res);
+                return res;
+            })
+        },
+    },
     mutations: {
-        logout: state => {
-            state.user.data ={};
+        logout: (state) => {
             state.user.token = null;
+            state.user.data ={};
+        },
+        setUser: (state, userData) => {
+            state.user.token = userData.token;
+            state.user.data = userData.user;
+            sessionStorage.setItem('TOKEN', userData.token);
         }
     },
     modules: {}
